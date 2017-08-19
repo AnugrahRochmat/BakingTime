df2eca175a34234685ccb2be6075184f67440f7f
Anugrah Rochmat <anugrah.rochmat@gmail.com>
2017-08-19 13:16:05 +0700

    ExoPlayer created and Espresso Test Recorder

--- app/src/androidTest/java/com/example/android/bakingtime/BakingTimeTest.java
+++ app/src/androidTest/java/com/example/android/bakingtime/BakingTimeTest.java
@@ -72,32 +72,6 @@ public class BakingTimeTest {
             e.printStackTrace();
         }
 
-        ViewInteraction textView = onView(
-                allOf(withText("Starting prep"), isDisplayed()));
-        textView.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ViewInteraction textView2 = onView(
-                allOf(withText("Melt butter and bittersweet chocolate."), isDisplayed()));
-        textView2.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         pressBack();
 
         pressBack();
@@ -123,21 +97,6 @@ public class BakingTimeTest {
                                 withParent(withId(R.id.action_bar_container)))),
                         isDisplayed()));
         appCompatImageButton3.perform(click());
-
-        ViewInteraction frameLayout = onView(
-                allOf(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class), isDisplayed()));
-        frameLayout.check(matches(isDisplayed()));
-
-        ViewInteraction frameLayout2 = onView(
-                allOf(withId(R.id.action_bar_container),
-                        childAtPosition(
-                                allOf(withId(R.id.decor_content_parent),
-                                        childAtPosition(
-                                                IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
-                                                0)),
-                                0),
-                        isDisplayed()));
-        frameLayout2.check(matches(isDisplayed()));
 
         ViewInteraction recyclerView5 = onView(
                 allOf(withId(R.id.recipes_recycler_view),
@@ -246,17 +205,6 @@ public class BakingTimeTest {
                 allOf(withId(R.id.recipe_ingredients_button), withText("Recipe Ingredients"), isDisplayed()));
         appCompatButton3.perform(click());
 
-        ViewInteraction textView3 = onView(
-                allOf(withText("Nutella Pie (Ingredients)"),
-                        childAtPosition(
-                                allOf(withId(R.id.action_bar),
-                                        childAtPosition(
-                                                withId(R.id.action_bar_container),
-                                                0)),
-                                1),
-                        isDisplayed()));
-        textView3.check(matches(withText("Nutella Pie (Ingredients)")));
-
         ViewInteraction recyclerView10 = onView(
                 allOf(withId(R.id.ingredients_recycler_view),
                         childAtPosition(
@@ -292,17 +240,6 @@ public class BakingTimeTest {
             e.printStackTrace();
         }
 
-        ViewInteraction textView4 = onView(
-                allOf(withText("Nutella Pie"),
-                        childAtPosition(
-                                allOf(withId(R.id.action_bar),
-                                        childAtPosition(
-                                                withId(R.id.action_bar_container),
-                                                0)),
-                                1),
-                        isDisplayed()));
-        textView4.check(matches(withText("Nutella Pie")));
-
         ViewInteraction viewPager = onView(
                 allOf(withId(R.id.view_pager),
                         childAtPosition(
@@ -322,86 +259,6 @@ public class BakingTimeTest {
                                 0),
                         isDisplayed()));
         frameLayout4.check(matches(isDisplayed()));
-
-        ViewInteraction imageButton = onView(
-                allOf(withId(R.id.exo_prev), withContentDescription("Previous track"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        0),
-                                0),
-                        isDisplayed()));
-        imageButton.check(matches(isDisplayed()));
-
-        ViewInteraction imageButton2 = onView(
-                allOf(withId(R.id.exo_rew), withContentDescription("Rewind"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        0),
-                                1),
-                        isDisplayed()));
-        imageButton2.check(matches(isDisplayed()));
-
-        ViewInteraction imageButton3 = onView(
-                allOf(withId(R.id.exo_pause), withContentDescription("Pause"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        0),
-                                2),
-                        isDisplayed()));
-        imageButton3.check(matches(isDisplayed()));
-
-        ViewInteraction imageButton4 = onView(
-                allOf(withId(R.id.exo_ffwd), withContentDescription("Fast forward"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        0),
-                                3),
-                        isDisplayed()));
-        imageButton4.check(matches(isDisplayed()));
-
-        ViewInteraction imageButton5 = onView(
-                allOf(withId(R.id.exo_next), withContentDescription("Next track"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        0),
-                                4),
-                        isDisplayed()));
-        imageButton5.check(matches(isDisplayed()));
-
-        ViewInteraction defaultTimeBar = onView(
-                allOf(withId(R.id.exo_progress), withContentDescription("00:00"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        1),
-                                1),
-                        isDisplayed()));
-        defaultTimeBar.check(matches(isDisplayed()));
-
-        ViewInteraction textView5 = onView(
-                allOf(withId(R.id.exo_position), withText("00:00"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        1),
-                                0),
-                        isDisplayed()));
-        textView5.check(matches(withText("00:00")));
-
-        ViewInteraction textView6 = onView(
-                allOf(withId(R.id.step_description), withText("Recipe Introduction"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        1),
-                                0),
-                        isDisplayed()));
-        textView6.check(matches(withText("Recipe Introduction")));
 
         ViewInteraction linearLayout = onView(
                 allOf(childAtPosition(
